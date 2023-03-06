a137ff9d51c07b59cff838e73788df26321ed3b6
Manuel Lamon <leo0lamon@gmail.com>
2023-03-05 22:40:37 -0400

    mejora en los iconos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "../utils/theme";
+import '@fortawesome/fontawesome-svg-core/styles.css'; 
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
